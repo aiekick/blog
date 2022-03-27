7b0cd067e9c6d14881b2219ad8c282acffeeeaa5
aiekick <aiekick@funparadigm.com>
2022-03-27 18:25:45 +0200

    -

---
+++ docusaurus.config.js
@@ -0,0 +1,156 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'FunParadigm',
+  tagline: 'Fascinated by programming and art under all theirs forms',
+  url: 'https://blog.funparadigm.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'FunParadigm', // Usually your GitHub org/user name.
+  projectName: 'FunParadigm', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://blog.funparadigm.com/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://blog.funparadigm.com/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    ({
+      navbar: {
+        title: 'Home',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tuto',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Android',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Softwares',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Libraries',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Various',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'right',
+            label: 'About',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Social Networks',
+            items: [
+              {
+                label: 'Github',
+				href: 'https://github.com/aiekick',
+              },
+              {
+                label: 'Twitter',
+				href: 'https://twitter.com/aiekick',
+              },
+              {
+                label: 'OpenSea',
+                href: 'https://opensea.io/accounts/Aiekick',
+              },
+              {
+                label: 'Linkedin',
+                href: 'https://www.linkedin.com/in/aiekick/',
+              },
+            ],
+          },
+          {
+            title: 'Creative Networks',
+            items: [
+              {
+                label: 'ShaderToy',
+                href: 'https://www.shadertoy.com/user/aiekick',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://www.instagram.com/aiekick',
+              },
+              {
+                label: 'ArtStation',
+                href: 'https://www.artstation.com/aiekick',
+              },
+              {
+                label: 'Sketchfab',
+                href: 'https://sketchfab.com/Aiekick/models',
+              },
+              {
+                label: 'Vimeo',
+                href: 'https://vimeo.com/aielevel',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} FunParadigm`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
